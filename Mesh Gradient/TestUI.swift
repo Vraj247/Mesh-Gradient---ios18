67343768f1665dff83659938fc1c7f6b87119be2
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2024-06-20 19:53:28 +0530

    Added the Material Design 2 Color System with Typography.

---
+++ Mesh Gradient/TestUI.swift
@@ -0,0 +1,18 @@
+//
+//  TestUI.swift
+//  Mesh Gradient
+//
+//  Created by Vikas Yadav on 20/06/24.
+//
+
+import SwiftUI
+
+struct TestUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestUI()
+}
